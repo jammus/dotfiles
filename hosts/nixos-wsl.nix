5ff3aa7d8fe2b94be1680b1da8f377ad7ec41207
jammus <jammus@gmail.com>
2022-07-02 03:52:01 +0000

    Make me the default user on wsl

--- hosts/nixos-wsl.nix
+++ hosts/nixos-wsl.nix
@@ -16,7 +16,7 @@ in
   wsl = {
     enable = true;
     automountPath = "/mnt";
-    defaultUser = "nixos";
+    defaultUser = "jammus";
     startMenuLaunchers = true;
     wslConf.network.hostname = "nixos-wsl";
 
