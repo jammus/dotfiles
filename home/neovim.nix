f93c38a5d59a48171996642b35e1bbe5e2638a27
jammus <jammus@gmail.com>
2022-07-03 08:40:24 +0000

    Import my vim preferences

--- home/neovim.nix
+++ home/neovim.nix
@@ -5,9 +5,24 @@
     enable = true;
     viAlias = true;
     vimAlias = true;
-
     plugins = with pkgs; [
       vimPlugins.vim-nix
+      vimPlugins.vim-surround
     ];
+    extraConfig = ''
+      let mapleader = ","
+
+      set ignorecase
+
+      set ruler
+      set number
+      set relativenumber
+
+      nnoremap <leader><space> :noh<cr>
+
+      " use tab to match bracket pairs
+      nnoremap <tab> %
+      vnoremap <tab> %
+    '';
   };
 }
