2a9b33381758d166766a3fc65fda27158aaae501
jammus <jammus@gmail.com>
2022-07-18 14:20:33 +0800

    Configure git colours and behaviour

--- home/git.nix
+++ home/git.nix
@@ -3,5 +3,22 @@
     enable = true;
     userName = "jammus";
     userEmail = "jammus@gmail.com";
+    extraConfig = {
+      color = {
+        branch = "auto";
+        diff = "auto";
+        status = {
+          added = "green";
+          changed = "yellow";
+          untracked = "red";
+        };
+      };
+      pull = {
+        rebase = true;
+      };
+      rebase = {
+        autosquash = true;
+      };
+    };
   };
 }
