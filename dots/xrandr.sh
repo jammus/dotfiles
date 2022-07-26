24b50ab83f9acb7e0f0afcc20c5cd54cd6321a10
jammus <jammus@gmail.com>
2022-07-26 22:16:32 +0800

    Revert scaling

--- dots/xrandr.sh
+++ dots/xrandr.sh
@@ -1,2 +1,2 @@
 xrandr --output eDP-1-1 --mode 3840x2160 --rate 60 --primary --scale 0.5
-xrandr --output DP-1-1 --left-of eDP-1-1 --mode 2560x1440 --scale 0.999x0.999
+xrandr --output DP-1-1 --left-of eDP-1-1 --mode 2560x1440 --scale-from 2560x1400
