f888794af978aa5d6e77bba32b750a1b08b6a6ab
jammus <jammus@gmail.com>
2022-07-26 22:43:24 +0800

    Setup tailscale for moosebird

--- hosts/moosebird/configuration.nix
+++ hosts/moosebird/configuration.nix
@@ -79,14 +79,16 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
-  # This will save you money and possibly your life!
-  services.thermald.enable = true;
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  services.tailscale.enable = true;
+
+  # This will save you money and possibly your life!
+  services.thermald.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
