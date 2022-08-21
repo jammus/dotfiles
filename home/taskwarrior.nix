c36d7d8a54ca8f6bbbe7b53cf452364a93ad0125
jammus <jammus@gmail.com>
2022-08-21 08:37:20 +0000

    Enable taskwarrior sync

--- home/taskwarrior.nix
+++ home/taskwarrior.nix
@@ -1,5 +1,11 @@
 {
   programs.taskwarrior = {
     enable = true;
+    config = {
+      taskd = {
+        server = "100.104.40.62:53589";
+        trust = "ignore hostname";
+      };
+    };
   };
 }
