3825c16126a2d04ae75931ad82342676bc55a82d
jammus <jammus@gmail.com>
2022-08-21 07:31:26 +0000

    Fix issue with nix/homemanager mismatch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
