f1853e42de0778e53d19e7d9ed6435366c153b02
jammus <jammus@gmail.com>
2022-08-23 19:59:30 +0800

    Prevent issue with nm-online breaking rebuild
    See https://github.com/NixOS/nixpkgs/issues/180175

--- common/networking.nix
+++ common/networking.nix
@@ -3,6 +3,10 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Disable wait online as it's causing trouble at rebuild
+  # See: https://github.com/NixOS/nixpkgs/issues/180175
+  systemd.services.NetworkManager-wait-online.enable = false;
+
   services.tailscale.enable = true;
 
   services.avahi = {
