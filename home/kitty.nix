627d7a0793ffd37ea0c58d132f8a82140c2b5612
jammus <jammus@gmail.com>
2022-09-19 13:10:22 +0800

    Configure Kitty theme

--- home/kitty.nix
+++ home/kitty.nix
@@ -1,5 +1,13 @@
 {
   programs.kitty = {
     enable = true;
+    theme = "Gruvbox Dark Soft";
+    font = {
+      name = "monospace";
+      size = 14;
+    };
+    extraConfig = ''
+      draw_minimal_borders no
+    '';
   };
 }
