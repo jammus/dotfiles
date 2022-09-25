4afdad076d7397f2eceaac384367dd7330f40481
jammus <jammus@gmail.com>
2022-09-25 17:24:35 +0800

    Enable sound on gianthead

--- hosts/gianthead/configuration.nix
+++ hosts/gianthead/configuration.nix
@@ -70,6 +70,16 @@
     xkbVariant = "";
   };
 
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
