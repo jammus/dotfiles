2fce7119749ce772b02e52b9e5cd331a04cf9cf5
jammus <jammus@gmail.com>
2022-09-25 19:50:49 +0800

    Use picom to fancy up the windows

--- home/xmonad.nix
+++ home/xmonad.nix
@@ -5,4 +5,26 @@
     enableContribAndExtras = true;
     config = ./xmonad.hs;
   };
+
+  services.picom = {
+    enable = true;
+
+    activeOpacity = 1.0;
+    inactiveOpacity = 0.95;
+
+    settings = {
+      blur = {
+        method = "gaussian";
+        size = 10;
+        deviation = 5.0;
+      };
+    };
+
+    experimentalBackends = true;
+
+    opacityRules = [
+      "100:class_g *?= 'Rofi'"
+      "100:class_g *?= 'Firefox'"
+    ];
+  };
 }
