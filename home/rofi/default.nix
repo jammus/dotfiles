84a0668217592002d63afa1292ecc0749b58f723
jammus <jammus@gmail.com>
2022-09-26 22:27:08 +0800

    Neaten gruvbox theme for Rofi

--- home/rofi/default.nix
+++ home/rofi/default.nix
@@ -4,6 +4,6 @@
     enable = true;
     package = with pkgs; rofi.override { plugins = [ rofi-calc rofi-emoji ]; };
     terminal = "${pkgs.kitty}/bin/kitty";
-    theme = ./gruvbox-dark-hard.rasi;
+    theme = ./theme.rasi;
   };
 }
