fb328a409809ea972ea64db588569c70cbddd54c
jammus <jammus@gmail.com>
2022-09-30 11:55:51 +0800

    Apply some system defaults

--- common/darwin.nix
+++ common/darwin.nix
@@ -10,4 +10,10 @@
 
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToEscape = true;
+
+  system.defaults.dock.autohide = true;
+
+  system.defaults.spaces.spans-displays = false;
+  system.defaults.dock.mru-spaces = false;
+  system.defaults.dock.expose-animation-duration = 0.1;
 }
