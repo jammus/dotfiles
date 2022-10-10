788b14831ee85b36a2f4c8379e66df28ac5ad53c
jammus <jammus@gmail.com>
2022-10-10 09:23:16 +0800

    Update some neovim lsp config

--- home/neovim.nix
+++ home/neovim.nix
@@ -26,6 +26,12 @@ in
       nvim-web-devicons
       nvim-lspconfig
       nvim-lazy-lsp
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp-buffer
+      cmp-vsnip
+      cmp-path
+      cmp-treesitter
       (nvim-treesitter.withPlugins (plugins: pkgs.tree-sitter.allGrammars))
     ];
     extraConfig = ''
@@ -55,6 +61,37 @@ in
       nnoremap <leader>F :NvimTreeToggle<cr>
 
       lua << EOF
+      local opts = { noremap=true, silent=true }
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+      -- Use an on_attach function to only map the following keys
+      -- after the language server attaches to the current buffer
+      local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- Mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+      end
       require('nvim-treesitter.configs').setup {
           highlight = {
               enable = true,
@@ -75,7 +112,7 @@ in
           flags = {
             debounce_text_changes = 150,
           },
-          -- on_attach = on_attach,
+          on_attach = on_attach,
           -- capabilities = capabilities,
         },
         -- Override config for specific servers that will passed down to lspconfig setup.
