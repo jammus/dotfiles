1d3a340fd5fde5f0f371db8526f39f4692524ea3
jammus <jammus@gmail.com>
2023-02-06 17:03:28 +0800

    Autocomplete in neovim

--- home/neovim.nix
+++ home/neovim.nix
@@ -99,6 +99,36 @@
               additional_vim_regex_highlighting = false,
           },
       }
+
+      -- Set up nvim-cmp.
+      local cmp = require'cmp'
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
       require('lazy-lsp').setup {
         -- By default all available servers are set up. Exclude unwanted or misbehaving servers.
         excluded_servers = {
@@ -114,7 +144,7 @@
             debounce_text_changes = 150,
           },
           on_attach = on_attach,
-          -- capabilities = capabilities,
+            capabilities = capabilities,
         },
         -- Override config for specific servers that will passed down to lspconfig setup.
         configs = {
