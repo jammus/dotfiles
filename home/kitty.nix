f2e7e000b7d98b91212ca7355ecb15b2b422e226
jammus <jammus@gmail.com>
2023-03-07 09:03:59 +0800

    Tab colours

--- home/kitty.nix
+++ home/kitty.nix
@@ -12,6 +12,13 @@
       enable_audio_bell yes
       close_on_child_death yes
       askpass unless-set
+
+      tab_fade 1
+      active_tab_background #d8a657
+      active_tab_foreground #1d2021
+      active_tab_font_style normal
+      inactive_tab_foreground #d4be98
+      inactive_tab_background #504945
     '';
   };
 }
