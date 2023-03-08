f07782c05985f67f0b167bb422ae7e0746c87679
jammus <jammus@gmail.com>
2023-03-08 15:11:03 +0800

    Add cache subtitutes

--- common/base.nix
+++ common/base.nix
@@ -8,4 +8,13 @@
   nix.extraOptions = ''
     experimental-features = nix-command flakes
   '';
+
+  nix.settings.substituters = [
+    "https://nix-community.cachix.org"
+    "https://cache.nixos.org/"
+  ];
+
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 }
