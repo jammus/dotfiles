71767387e84db9d8d1ed28ef9d48af8667e1b2ba
jammus <jammus@gmail.com>
2023-04-03 09:34:22 +0800

    Upgrade some stuff

--- common/users.nix
+++ common/users.nix
@@ -2,6 +2,8 @@
 { config, pkgs, ... }:
 
 {
+  programs.zsh.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jammus = {
     isNormalUser = true;
--- hosts/giant-head/hardware-configuration.nix
+++ hosts/giant-head/hardware-configuration.nix
@@ -38,6 +38,4 @@
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
 }
