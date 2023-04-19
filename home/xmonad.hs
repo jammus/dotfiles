3ca8f9769623a9d075153d7ab30a02e3d376e0c4
jammus <jammus@gmail.com>
2023-04-19 18:20:39 +0800

    Go true fullscreen

--- home/xmonad.hs
+++ home/xmonad.hs
@@ -4,9 +4,11 @@ import XMonad.Actions.UpdatePointer
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
@@ -21,8 +23,12 @@ xmonadConfig = def {
     normalBorderColor = "#504945",
     focusedBorderColor = "#e78a43",
     borderWidth = 3,
-    layoutHook = avoidStruts $ spacing 10 (layoutHook defaultConfig ||| emptyBSP ||| ResizableTall 1 (3/100) (1/2) []),
+    layoutHook = smartBorders . avoidStruts $ spacing 10 (layoutHook defaultConfig ||| emptyBSP ||| ResizableTall 1 (3/100) (1/2) []),
     focusFollowsMouse = False,
+    manageHook = composeAll [
+        isFullscreen --> doFullFloat,
+        manageHook defaultConfig
+    ],
     logHook = updatePointer (0.5, 0.5) (0, 0)
   }
   `additionalKeysP`
