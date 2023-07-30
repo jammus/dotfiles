5b51c6fcd31a75b476eabc761208a798cf686359
jammus <jammus@gmail.com>
2023-07-30 22:57:12 +0800

    Add taskmaster to syncthing

--- roles/syncthing.nix
+++ roles/syncthing.nix
@@ -9,13 +9,16 @@
       guiAddress = "0.0.0.0:8384";
       overrideDevices = true;     # overrides any devices added or deleted through the WebUI
       overrideFolders = true;     # overrides any folders added or deleted through the WebUI
-      devices = {
-        "giant-head" = { id = "A42RQCE-7M67JCX-2VSR2TU-HLZXZVG-KAXTVP7-L6BS4OX-L5FYWVW-WUTDFQI"; };
-      };
-      folders = {
-        "nb" = {
-          path = "/home/jammus/nb";
-          devices = [ "giant-head" ];
+      settings = {
+        devices = {
+          "giant-head" = { id = "A42RQCE-7M67JCX-2VSR2TU-HLZXZVG-KAXTVP7-L6BS4OX-L5FYWVW-WUTDFQI"; };
+          "taskmaster" = { id = "2ECO2VV-2RWO6XR-6YNQTF5-4PDYNUV-L26NTQY-V7IJ5NR-UOAEUAV-VRYOSA7"; };
+        };
+        folders = {
+          "nb" = {
+            path = "/home/jammus/nb";
+            devices = [ "giant-head" "taskmaster" ];
+          };
         };
       };
     };
