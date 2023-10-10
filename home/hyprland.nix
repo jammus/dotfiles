582b49b54cff510e858b109bb696f45b9d1a4a54
jammus <jammus@gmail.com>
2023-10-10 23:14:22 +0800

    Use waybar under hyprland

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -22,4 +22,40 @@
   programs.wofi = {
     enable = true;
   };
+
+  programs.waybar = {
+    enable = true;
+    settings = [{
+      modules-left = [ "hyprland/workspaces" ];
+      modules-center = [ "hyprland/window" ];
+      modules-right = [ "temperature" "cpu" "memory" "pulseaudio" "clock" ];
+      pulseaudio = {
+        format = "{icon}  {volume}%";
+        format-muted = "󰖁";
+        format-icons = {
+          default = ["" "" ""];
+        };
+        on-click = "pavucontrol";
+      };
+      "wlr/workspaces" = {
+        active-only = false;
+        format = "{name}";
+        format-icons = {
+          "1" = "一";
+          "2" = "二";
+          "3" = "三";
+          "4" = "四";
+          "5" = "五";
+          "6" = "六";
+          "7" = "七";
+          "8" = "八";
+          "9" = "九";
+          "10" = "十";
+          # "urgent" = "";
+          # "active" = "";
+          # "default" = "";
+        };
+      };
+    }];
+  };
 } 
