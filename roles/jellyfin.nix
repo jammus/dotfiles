5d9ef1787a6272f4a6fbba2be104d123793193a9
jammus <jammus@gmail.com>
2024-01-06 01:45:34 +0800

    Jellyfin moved from container to service

--- roles/jellyfin.nix
+++ roles/jellyfin.nix
@@ -13,23 +13,8 @@
       intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
     ];
   };
+
   services.jellyfin.enable = true;
-
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-      # For Nixos version > 22.11
-      #defaultNetwork.settings = {
-      #  dns_enabled = true;
-      #};
-    };
-  };
 
   users.users = {
     jellyfin = {
