9ad4172039145870a5fbfc89b62ab572830877c9
jammus <jammus@gmail.com>
2024-01-08 21:41:15 +0800

    Podman dns setting switched to off
    Seems to prevent containers accessing the outside world

--- hosts/taskmaster/configuration.nix
+++ hosts/taskmaster/configuration.nix
@@ -152,7 +152,6 @@
       dockerSocket.enable = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
       # For Nixos version > 22.11
       #defaultNetwork.settings = {
       #  dns_enabled = true;
