8ee867657d691e3584bf835e37bd7a141789454b
jammus <jammus@gmail.com>
2024-01-21 16:09:20 +0800

    Obsidian needs insecure electron a while longer

--- home/default.nix
+++ home/default.nix
@@ -18,7 +18,12 @@
   ];
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      pkgs.lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0"
+    ];
+  };
 
   home.packages = with pkgs; [
     htop
