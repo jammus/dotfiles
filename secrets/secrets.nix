28fab8fb3924e3f4c879a66209b2ef5acb1167a8
jammus <jammus@gmail.com>
2024-03-18 23:55:27 +0800

    Secrets rekeyed

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,8 @@ let
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDCMGaXGzSmTVtHge03IKCx1UHvCUPHFCqRGuOTuJloCiaJSn0VOPkgPUhaGEji5huWvM3DqIA0BuqJmP6DgfAKhM39OjnvybixyBMTsXQa2z7wEYCZy5aSjnxzHV2jpfbI+dULYo8g9CzhOeDYx7qj+pkvgbUSucygeu8I1NYOl9+oXQjHp9PaT9u5wNoRXbdBQ5r0FINw+IijdnYL1fDGknylRFORWv/qyMau9nSPhe0oLNsnQE9N04DDCAj3xiL5YbbzOTZIVrwkb1aPrijHPERE2TBsv+u6YuZkvEBk0+le9SFysfDuZkSzHxyC+wmrIIsZ2kvTnlffQ3D0CQZFOSwrZKd7d04qt5L/b9wMXfvt6vzVdlZZcfwcMUCpvBrS6GydhiXzRD5Ymv5y9E2eNSwWwYCdVMB7xOKFfDRJUbQVhuJAadeTWGOJwWxGEMMpCu4MzrKwgnZ9ehmqefqfTp5xuhSsXRVEFdM9qZMEEYihvi5HYo2dbNCUfqdfAw0="  # jammus@gianthead
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBrMN78xfUybhtW0IqtFu41ZqoILsGS+39cMVDkNYx+S"  # jammus@taskmaster
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPYIfbjBP8hyIhqwg+sBSL0mCZ4+/1uzZ5ndj/7qe13A" # jammus@giant-head (2024)
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKI/Fa2TPriXo9wFuIekjy0ZeY6/r5O52MEbVI+nKWx" # op
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJOIdnD23MLA/0op5ZNuMzC6WE3FzCAwwKGtCaOvwhme" # op
   ];
   hosts = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPh+5vY1kvt2be0uIaQeN8WmHfcYiKuDogdSi04ctG1g"  # giant-head (olde)
