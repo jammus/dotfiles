7b358d646a03d12b81d43aaaca413d8c139c6acc
jammus <jammus@gmail.com>
2024-03-31 16:02:18 +0800

    Fix issue with devenv cache

--- common/base.nix
+++ common/base.nix
@@ -18,5 +18,9 @@
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
   ];
 
+  nix.settings.trusted-users = [
+    "jammus"
+  ];
+
   nix.settings.auto-optimise-store = true;
 }
