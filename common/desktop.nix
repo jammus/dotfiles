2f967b794968c1af8c54a925d4ec63e455b7b567
jammus <jammus@gmail.com>
2024-05-25 13:21:47 +0800

    SDDM renamed

--- common/desktop.nix
+++ common/desktop.nix
@@ -9,7 +9,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.windowManager.xmonad = {
     enable = true;
   };
