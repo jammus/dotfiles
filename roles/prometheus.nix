45e1dbcc28fdd80aa04fe5483e96d18b447246f7
jammus <jammus@gmail.com>
2024-06-01 10:45:24 +0800

    Fix up

---
+++ roles/prometheus.nix
@@ -0,0 +1,14 @@
+{ pkgs, config, ... }:
+{
+  services.prometheus = {
+    enable = true;
+    port = 9001;
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+        port = 9002;
+      };
+    };
+  };
+}
