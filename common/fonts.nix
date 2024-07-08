46d8c2caef23a2962f9e75a58bfc0f40acc1d15f
jammus <jammus@gmail.com>
2024-07-09 00:03:12 +0800

    Better Chinese fonts on linux

--- common/fonts.nix
+++ common/fonts.nix
@@ -5,6 +5,12 @@
     fontDir.enable = true;
     packages = with pkgs; [
       (nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
+      unifont
+      noto-fonts
+      noto-fonts-extra
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
     ];
   };
 }
