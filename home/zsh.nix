d7a46420a47c3fabc49e96eba7053837f66209e6
jammus <jammus@gmail.com>
2024-08-02 10:41:06 +0800

    Brew added to path

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,9 +1,13 @@
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     autosuggestion = {
       enable = true;
     };
+    initExtra = if pkgs.stdenv.hostPlatform.isDarwin then ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '' else "";
     enableCompletion = true;
     shellAliases = {
       ls = "lsd";
