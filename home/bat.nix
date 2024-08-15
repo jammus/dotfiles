a82b5f49093700e47cf149fdeddc65b288bd09f5
jammus <jammus@gmail.com>
2024-08-15 23:25:24 +0800

    Fennel support in bat

--- home/bat.nix
+++ home/bat.nix
@@ -1,8 +1,20 @@
+{ pkgs, ... }:
 {
   programs.bat = {
     enable = true;
     config = {
       theme = "gruvbox-dark";
+    };
+    syntaxes = {
+      fennel = {
+        src = pkgs.fetchFromGitHub {
+          owner = "gbaptista";
+          repo = "sublime-text-fennel";
+          rev = "v0.1.2";
+          hash = "sha256-puHDk0xDvdOfNGOkuc4AqaE/fSNm5vVFqoaFkL1vXIY=";
+        };
+        file = "Fennel.sublime-syntax";
+      };
     };
   };
 }
