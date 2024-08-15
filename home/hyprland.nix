bcc3f4a716bcbafe13471757761591c5b47d99a7
jammus <jammus@gmail.com>
2024-08-15 23:25:13 +0800

    Hyprfocus install

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -3,10 +3,14 @@
   xdg.configFile."hypr/hyprlock.conf".source = ../config/hyprlock.conf;
 
   xdg.configFile."hypr/hypridle.conf".source = ../config/hypridle.conf;
+
   xdg.configFile."waybar/style.css".source = ../config/waybar.css;
 
   wayland.windowManager.hyprland = {
     enable = true;
+    plugins = [
+      pkgs.hyprlandPlugins.hyprfocus 
+    ];
     settings = {
       "$mainMod" = "SUPER";
 
@@ -170,6 +174,38 @@
         "$mainMod,mouse_down,workspace,e+1"
         "$mainMod,mouse_up,workspace,e-1"
       ];
+
+      "plugin:hyprfocus" = {
+        enabled = "yes";
+
+        keyboard_focus_animation = "shrink";
+        mouse_focus_animation = "shrink";
+
+        bezier = [
+          "bezIn, 0.5,0.0,1.0,0.5"
+          "bezOut, 0.0,0.5,0.5,1.0"
+        ];
+
+        flash = {
+          flash_opacity = 0.9;
+
+          in_bezier = "bezIn";
+          in_speed = 0.5;
+
+          out_bezier = "bezOut";
+          out_speed = 3;
+        };
+
+        shrink = {
+          shrink_percentage = 0.8;
+
+          in_bezier = "bezIn";
+          in_speed = 0.5;
+
+          out_bezier = "bezOut";
+          out_speed = 3;
+        };
+      };
     };
   };
 
@@ -239,6 +275,7 @@
       };
     }];
   };
+
   programs.hyprlock.enable = true;
   services.hypridle.enable = true;
 } 
