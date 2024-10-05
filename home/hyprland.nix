32127f54cfe5e13d99963505e002d64dc30e142d
jammus <jammus@gmail.com>
2024-10-05 13:41:26 +0800

    Disable hyprfocus and animations

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -9,7 +9,6 @@
   wayland.windowManager.hyprland = {
     enable = true;
     plugins = [
-      pkgs.hyprlandPlugins.hyprfocus 
     ];
     settings = {
       "$mainMod" = "SUPER";
@@ -83,7 +82,7 @@
       };
 
       animations = {
-          enabled = 1;
+          enabled = 0;
           animation = [
             "windows,1,5,default"
             "border,1,10,default"
@@ -175,37 +174,7 @@
         "$mainMod,mouse_up,workspace,e-1"
       ];
 
-      "plugin:hyprfocus" = {
-        enabled = "yes";
 
-        keyboard_focus_animation = "shrink";
-        mouse_focus_animation = "shrink";
-
-        bezier = [
-          "bezIn, 0.5,0.0,1.0,0.5"
-          "bezOut, 0.0,0.5,0.5,1.0"
-        ];
-
-        flash = {
-          flash_opacity = 0.9;
-
-          in_bezier = "bezIn";
-          in_speed = 0.5;
-
-          out_bezier = "bezOut";
-          out_speed = 3;
-        };
-
-        shrink = {
-          shrink_percentage = 0.8;
-
-          in_bezier = "bezIn";
-          in_speed = 0.5;
-
-          out_bezier = "bezOut";
-          out_speed = 3;
-        };
-      };
     };
   };
 
