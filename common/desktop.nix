469ab6a7e6cb64c3b505c113d735c31da0904a75
jammus <jammus@gmail.com>
2025-02-02 00:12:35 +0800

    Use Plasma 6

--- common/desktop.nix
+++ common/desktop.nix
@@ -10,11 +10,15 @@
   services.xserver.enable = true;
 
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.xserver.windowManager.xmonad = {
     enable = true;
   };
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   security.polkit.enable = true;
   programs.sway.enable = true;
+
+  services.desktopManager.cosmic.enable = false;
+  services.displayManager.cosmic-greeter.enable = false;
 }
