107b78c52d51c5abd4c0375f89da6eafd3ed4e67
jammus <jammus@gmail.com>
2025-02-02 00:16:54 +0800

    Additional cachix

--- common/base.nix
+++ common/base.nix
@@ -12,10 +12,14 @@
   nix.settings.substituters = [
     "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
+    "https://cuda-maintainers.cachix.org"
+    "https://cosmic.cachix.org/"
   ];
 
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
   ];
 
   nix.settings.trusted-users = [
