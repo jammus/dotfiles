5adcbdbaa582d2d2edc2a0a64e8f8c3f53a5645e
jammus <jammus@gmail.com>
2025-12-30 10:19:13 +0800

    Leap fixed after add_default_mappings deprecated

--- home/neovim.nix
+++ home/neovim.nix
@@ -498,7 +498,8 @@
       { 
         plugin = leap-nvim; type = "lua";
         config = ''
-          require('leap').add_default_mappings()
+          vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+          vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
         '';
       }
       {
