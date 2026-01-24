aebe46c6c40b87e18b95355439d2d65bccfac386
jammus <jammus@gmail.com>
2026-01-24 14:55:19 +0800

    llama-cpp experiment
    Currently disabled

--- home/linux.nix
+++ home/linux.nix
@@ -1,7 +1,19 @@
 { pkgs, ... }:
+let
+  llama-cpp = pkgs.llama-cpp.overrideAttrs(attrs: rec {
+    version = "5869";
+    src = pkgs.fetchFromGitHub {
+      owner = "ggml-org";
+      repo = "llama.cpp";
+      tag = "b${version}";
+      hash = "sha256-sQK5OHuzRaT5wiz6+6ZBQxpzCLhYjdWy1ZsPcLrvMe4=";
+    };
+  });
+in
 {
-  home.packages = with pkgs; [
-    emacs
-    grc
+  home.packages = [
+    # (llama-cpp.override { cudaSupport = true; })
+    pkgs.emacs
+    pkgs.grc
   ];
 }
