3325dad68560a8b4ea2bd55f0e8dad901a4558c0
jammus <jammus@gmail.com>
2026-05-09 16:25:18 +0800

    Install some extra cachix

--- common/base.nix
+++ common/base.nix
@@ -9,17 +9,18 @@
     "flakes"
   ];
 
-  nix.settings.substituters = [
+  nix.settings.extra-substituters = [
     "https://nix-community.cachix.org"
-    "https://cache.nixos.org/"
     "https://cuda-maintainers.cachix.org"
     "https://cosmic.cachix.org/"
+    "https://devenv.cachix.org"
   ];
 
-  nix.settings.trusted-public-keys = [
+  nix.settings.extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
     "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+    "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw"
   ];
 
   nix.settings.trusted-users = [
