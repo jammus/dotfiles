bfe67c909ef7a5f917e2fd6bfd765932679cbb88
jammus <jammus@gmail.com>
2026-07-26 14:32:17 +0800

    Agent role templatised

--- roles/agent.nix
+++ roles/agent.nix
@@ -1,77 +1,86 @@
 { pkgs, inputs, config, ... }:
 let
   publicKeys = import ../common/public-keys.nix;
+  allowedDomains = [
+    "cache.nixos.org"
+    "nix-community.cachix.org"
+    "devenv.cachix.org"
+    "api.anthropic.com"
+    "console.anthropic.com"
+    "github.com"
+    "api.github.com"
+    "raw.githubusercontent.com"
+    "nixos.org"
+    "channels.nixos.org"
+    "codeload.github.com"
+    "files.pythonhosted.org"
+    "static.crates.io"
+    "huggingface.co"
+    "cas-server.xethub.hf.co"
+    "transfer.xethub.hf.co"
+    "pypi.org"
+    "registry.npmjs.org"
+    "repo1.maven.org"
+    "repo.clojars.org"
+  ];
+  allowedIps = [
+    "192.168.88.0/24"
+    "192.168.100.0/24"
+  ];
+  systemPackages = [
+    pkgs.claude-code
+    pkgs.bind
+    pkgs.git
+    pkgs.direnv
+    pkgs.devenv
+    pkgs.vim
+    inputs.backlog-md.packages.x86_64-linux.default
+  ];
+  imports = [
+    ../home/default.nix
+    ../home/emacs.nix
+  ];
+  interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+  '';
+  hosts = {
+    "192.168.100.10" = ["taskmaster"];
+  };
 in
 {
-  imports = [ ../modules/dev-container.nix ];
+  imports = [
+    ../modules/dev-container.nix
+  ];
   
   devContainers.agent-host = {
     hostAddress = "192.168.100.10";
     localAddress = "192.168.100.11";
     enableFirewallFiltering = true;
-    allowedDomains = [
-      "cache.nixos.org"
-      "nix-community.cachix.org"
-      "devenv.cachix.org"
-      "api.anthropic.com"
-      "console.anthropic.com"
-      "github.com"
-      "api.github.com"
-      "raw.githubusercontent.com"
-      "nixos.org"
-      "channels.nixos.org"
-      "codeload.github.com"
-      "files.pythonhosted.org"
-      "static.crates.io"
-      "huggingface.co"
-      "cas-server.xethub.hf.co"
-      "transfer.xethub.hf.co"
-      "pypi.org"
-      "registry.npmjs.org"
-      "repo1.maven.org"
-      "repo.clojars.org"
-    ];
-    allowedIps = [
-      "192.168.88.0/24"
-      "192.168.100.0/24"
-    ];
+    allowedDomains = allowedDomains;
+    allowedIps = allowedIps;
     config = {
       imports = [
         inputs.home-manager.nixosModules.home-manager
       ];
       home-manager.useUserPackages = true;
       home-manager.users.agent = {
-        imports = [
-          ../home/default.nix
-        ];
+        imports = imports;
       };
-      environment.systemPackages = [
-        pkgs.claude-code
-        pkgs.bind
-        pkgs.git
-        pkgs.direnv
-        pkgs.devenv
-        pkgs.vim
-        inputs.backlog-md.packages.${pkgs.stdenv.hostPlatform.system}.default
-      ];
+      environment.systemPackages = systemPackages;
       services = {
         openssh.enable = true;
       };
       programs = {
         fish.enable = true;
         bash = {
-          interactiveShellInit = ''
-            if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-            then
-              shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-              exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-            fi
-          '';
+          interactiveShellInit = interactiveShellInit;
         };
       };
-      networking.hosts = {
-        "192.168.100.10" = [ config.networking.hostName ];
-      };
+      networking.hosts = hosts;
       users.users.agent = {
         uid = 1047;
         initialHashedPassword = "*";
@@ -95,19 +104,9 @@ in
       ];
       home-manager.useUserPackages = true;
       home-manager.users.agent = {
-        imports = [
-          ../home/default.nix
-        ];
+        imports = imports;
       };
-      environment.systemPackages = [
-        pkgs.claude-code
-        pkgs.bind
-        pkgs.git
-        pkgs.direnv
-        pkgs.devenv
-        pkgs.vim
-        inputs.backlog-md.packages.${pkgs.stdenv.hostPlatform.system}.default
-      ];
+      environment.systemPackages = systemPackages;
       services = {
         openssh.enable = true;
       };
@@ -123,9 +122,7 @@ in
           '';
         };
       };
-      networking.hosts = {
-        "192.168.100.10" = [ config.networking.hostName ];
-      };
+      networking.hosts = hosts;
 			networking.resolvconf.enable = false;
 			environment.etc."resolv.conf".text = "nameserver 1.1.1.1";
       users.users.agent = {
