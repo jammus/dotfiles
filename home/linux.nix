d8639f89357e4102026b8fbaae0f6fa5d8563b02
jammus <jammus@gmail.com>
2026-08-13 18:32:28 +0800

    llama-cpp override removed
    Not being used right now

--- home/linux.nix
+++ home/linux.nix
@@ -1,15 +1,4 @@
 { pkgs, inputs, ... }:
-let
-  llama-cpp = pkgs.llama-cpp.overrideAttrs(attrs: rec {
-    version = "5869";
-    src = pkgs.fetchFromGitHub {
-      owner = "ggml-org";
-      repo = "llama.cpp";
-      tag = "b${version}";
-      hash = "sha256-sQK5OHuzRaT5wiz6+6ZBQxpzCLhYjdWy1ZsPcLrvMe4=";
-    };
-  });
-in
 {
   imports = [
     ./emacs.nix
