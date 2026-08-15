9aee2a863616e1ebd3025c33206409da9bb7e605
jammus <jammus@gmail.com>
2026-08-15 12:22:03 +0800

    Missing usbhid on giant-head

--- hosts/taskmaster/hardware-configuration.nix
+++ hosts/taskmaster/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
