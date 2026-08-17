021148050ac1c93ecaae82adb1b5699a9bc2174b
jammus <jammus@gmail.com>
2026-08-17 10:07:04 +0800

    LLM and Ghostty for Darwin

--- home/darwin-desktop.nix
+++ home/darwin-desktop.nix
@@ -1,13 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   imports = [
     ./kitty.nix
     ./emacs.nix
+    ./ghostty.nix
+    ./llms.nix
   ];
 
   home.packages = with pkgs; [
     viu
-    jetbrains.idea-ultimate
+    # jetbrains.idea-ultimate
     obsidian
+    grc
+    # virtualbox
+    claude-code
+    # inputs.backlog-md.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
--- home/ghostty.nix
+++ home/ghostty.nix
@@ -8,7 +8,7 @@ in
     package = if pkgs.stdenv.hostPlatform.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
     settings = {
       theme = "Gruvbox Material Dark";
-      window-decoration = "none";
+      window-decoration = if pkgs.stdenv.isDarwin then "auto" else "none";
       font-family = font.family;
       font-size = font.size;
     };
